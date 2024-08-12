ccac59c998a0f5c5391506fc44acb20413da7378
oumnya <oumnya.benhassou@gmail.com>
2024-08-12 20:05:43 +0000

    fix swagger

--- src/main/java/com/cloudstation/JavaExampleApplication.java
+++ src/main/java/com/cloudstation/JavaExampleApplication.java
@@ -2,9 +2,13 @@ package com.cloudstation.javaexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Books API", version = "1.0", description = "Books Information"))
 public class JavaExampleApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(JavaExampleApplication.class, args);
     }
